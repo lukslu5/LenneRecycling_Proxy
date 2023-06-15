40d2ad3b6bf25b8851f0e8dd53281a743003f893
lukslu5 <luca-stinn@web.de>
2023-06-15 10:57:15 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const http = require('http');
+const querystring = require('querystring');
+
+const app = express();
+
+// Enable CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+// Endpoint to handle the API request
+app.get('/api', (req, res) => {
+  const barcode = req.query.ean;
+  const queryParams = querystring.stringify({
+    ean: barcode,
+    cmd: 'query',
+    queryid: '400000000',
+  });
+
+  // Options for the target API request
+  const options = {
+    hostname: 'opengtindb.org',
+    path: `/?${queryParams}`,
+    method: 'GET',
+  };
+
+  // Make the request to the target API
+  const request = http.request(options, (response) => {
+    let data = '';
+
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    response.on('end', () => {
+      res.send(data);
+    });
+  });
+
+  request.on('error', (error) => {
+    console.error(error);
+    res.status(500).send('An error occurred');
+  });
+
+  request.end();
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
